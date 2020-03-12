90937ddcaa0d9ea9cc41aa0f845e78965504f95a
Angélica <agempka@gmail.com>
2020-03-12 07:59:13 -0300

    probando pull

--- app/src/main/java/com/example/apptallerya/Main2Activity.java
+++ app/src/main/java/com/example/apptallerya/Main2Activity.java
@@ -1,21 +1,33 @@
 package com.example.apptallerya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements  PerfilesTalleresFragment.OnFragmentInteractionListener {
     public static final String nombre_cliente="nombre_cliente";
     TextView txtBienvenido;
+    Button btnTalleres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-       // txtBienvenido=(TextView)findViewById(R.id.txtbienvenido);
-        //String cliente=getIntent().getStringExtra("nombre_cliente");
-        //txtBienvenido.setText("¡Bienvenido "+ cliente + "!");
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.escenario2, new PerfilesTalleresFragment()).commit();
+
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
