601c93c561d0a1c8a35bfcf2d12291fd08d7bb83
Sebastián <bordonsebastian25@gmail.com>
2020-03-21 17:22:39 -0300

    Splash

---
+++ app/src/main/java/com/example/apptallerya/Splash.java
@@ -0,0 +1,33 @@
+package com.example.apptallerya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        Thread th = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        th.start();
+    }
+}
